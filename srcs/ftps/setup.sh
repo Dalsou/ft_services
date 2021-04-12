1a261370aed3436287ffe7b8ab7f678ee28ab59d
Amélie Foulquier <amelie.foulquier@gmail.com>
2021-04-12 15:36:15 +0200

    ftps & phpma not ok

--- srcs/ftps/setup.sh
+++ srcs/ftps/setup.sh
@@ -6,10 +6,15 @@ openssl req -newkey rsa:2048 -nodes -days 365 -x509 \
 -keyout /etc/ssl/private/vsftpd-selfsigned.key \
 -out /etc/ssl/certs/vsftpd-selfsigned.crt
 
+# Create directory
+mkdir -p /var/ftps
+
 # Create user
-mkdir -p /var/ftp
-adduser -D -h /var/ftp user
+adduser -D -h /var/ftps user
 echo "user:password" | chpassword
+
+# Create test file
+echo "Hello World !" > /var/ftps/hello.txt
 
 # Start Telegraf
 telegraf & 
