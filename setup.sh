aa87cfc8b9ae433bcdd899f9e359da7404348f75
Amélie Foulquier <amelie.foulquier@gmail.com>
2021-04-13 22:38:09 +0200

    php proxy ok

--- setup.sh
+++ setup.sh
@@ -24,16 +24,23 @@ minikube dashboard &
 # # Build docker images
 echo "Docker build init ..."
 docker build -t my_nginx ./srcs/nginx > /dev/null 2>&1
+echo "Nginx completed !"
 docker build -t my_ftps ./srcs/ftps > /dev/null 2>&1
+echo "FTPS completed !"
 docker build -t my_mysql ./srcs/mysql > /dev/null 2>&1
+echo "MySQL completed !"
 docker build -t my_wordpress ./srcs/wordpress > /dev/null 2>&1
+echo "Wordpress completed !"
 docker build -t my_phpmyadmin ./srcs/phpmyadmin > /dev/null 2>&1
+echo "PHPmyAdmin completed !"
 docker build -t my_grafana ./srcs/grafana > /dev/null 2>&1
+echo "Grafana completed !"
 docker build -t my_influxdb ./srcs/influxdb > /dev/null 2>&1
+echo "InfluxDB completed !"
 echo "Docker build completed !"
 
 # Deploy service
-echo "Deploy init ..."
+echo "Deployment init ..."
 kubectl apply -f ./srcs/nginx.yaml
 kubectl apply -f ./srcs/ftps.yaml
 kubectl apply -f ./srcs/mysql.yaml
@@ -41,6 +48,6 @@ kubectl apply -f ./srcs/wordpress.yaml
 kubectl apply -f ./srcs/phpmyadmin.yaml
 kubectl apply -f ./srcs/grafana.yaml
 kubectl apply -f ./srcs/influxdb.yaml
-echo "Deploy completed !"
+echo "Deployment completed !"
 
 
